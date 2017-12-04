4484fe9ac29355a2522d6c99db2d55c61f65812f
armogidec <chudak_93@mail.ru>
2017-12-04 19:00:15 +0500

    modify for 2nd part

--- day2/script2.py
+++ day2/script2.py
@@ -1,9 +1,13 @@
-file = open('input.txt')
+'''Script for 2nd day task'''
+F = open('day2/input.txt')
 
-sum = 0
+SUM = 0
 
-for line in file:
+for line in F:
     a = [int(i) for i in line.split()]
-    sum += (max(a) - min(a))
+    for i, val1 in enumerate(a):
+        for j, val2 in enumerate(a):
+            if val1 % val2 == 0 and i != j:
+                SUM += val1 / val2
 
-print(sum)
+print SUM
