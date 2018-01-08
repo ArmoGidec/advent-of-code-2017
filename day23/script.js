77e23d1079ef3f9eafa9390ae96a41addbf3b41e
armogidec <chudak_93@mail.ru>
2018-01-09 03:28:56 +0500

    day 23 part 2

--- day23/script.js
+++ day23/script.js
@@ -6,6 +6,7 @@ fs.readFile('day23/input.txt', (err, data) => {
         return;
     }
     main(data.toString());
+    part2();
 });
 
 let CPU = {
@@ -13,6 +14,27 @@ let CPU = {
     registers: {},
     mul_invoked: 0
 };
+
+function part2() {
+    /*
+    for x in range(105700,122700 + 1,17):
+        for i in range(2,x):
+            if x % i == 0:
+                h += 1
+                break
+    print(h)
+    */
+    let h = 0;
+    for (let x = 109300; x < 126300 + 1; x += 17) {
+        for (let i = 2; i < x; i++) {
+            if (x % i == 0) {
+                h += 1;
+                break;
+            }
+        }
+    }
+    console.log(`h = ${h}`);
+}
 
 function main(text) {
     "abcdefgh".split('').forEach(register => CPU.registers[register] = 0);
@@ -25,24 +47,28 @@ function main(text) {
 }
 
 function set(register, val) {
-    val = parseToInt(val) || CPU.registers[val];
+    let new_val = parseToInt(val);
+    val = new_val !== undefined ? new_val : CPU.registers[val];
     CPU.registers[register] = val;
 }
 
 function sub(register, val) {
-    val = parseToInt(val) || CPU.registers[val];
+    let new_val = parseToInt(val);
+    val = new_val !== undefined ? new_val : CPU.registers[val];
     CPU.registers[register] -= val;
 }
 
 function mul(register, val) {
-    val = parseToInt(val) || CPU.registers[val];
-    CPU.registers[register] *= val;
+    let new_val = parseToInt(val);
+    val = new_val !== undefined ? new_val : CPU.registers[val];
     CPU.mul_invoked += 1;
 }
 
 function jnz(register, val) {
     val = parseInt(val);
-    if (CPU.registers[register] !== 0) {
+    let new_val = parseToInt(register);
+    let check_val = new_val !== undefined ? new_val : CPU.registers[register];
+    if (check_val !== 0) {
         CPU.i += val - 1;
     }
 }
